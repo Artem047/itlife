b61362d1ec0a4a72db363df32e44e431f9ee5e3e
Artem047 <artem.glebov2004@yandex.ru>
2023-04-17 17:58:52 +0300

    complete header

--- itlife_react/src/components/Header/Header.jsx
+++ itlife_react/src/components/Header/Header.jsx
@@ -1,23 +1,25 @@
-import Logo from "../images/Logo.png"
+import Logo from "../../images/Logo.png"
+import s from './Header.module.css'
 
 const Header = () => {
     return (
         <div>
-            <div className="header">
-                <ul className="menu_left">
+            <div className={s.header}>
+                <ul className={s.menu_left}>
                     <img src={Logo} alt="" />
                     <p>ITLIFE</p>
                 </ul>
-                <ul className="menu_center">
+                <ul className={s.menu_center}>
                     <li><a href="#">HOME</a></li>
                     <li><a href="#">SERVICES</a></li>
                     <li><a href="#">ABOUT</a></li>
                     <li><a href="#">CONTACTS</a></li>
                 </ul>
-                <ul className="menu_right">
+                <ul className={s.menu_right}>
                     <p>CALLBACK</p>
                 </ul>
             </div>
+            <hr />
         </div>
     )
 }
